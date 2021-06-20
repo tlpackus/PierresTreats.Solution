bf92a175a7f2683652588e3b4fa3ddb87c6a804b
tlpackus <tlpackus@gmail.com>
2021-06-19 21:49:36 -0700

    add flavor create and view list routs and views

---
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresTreats.Controllers
+{
+  public class FlavorsController: Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public FlavorsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
